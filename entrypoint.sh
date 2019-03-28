26209404ff18ce361c3e6cd57cbc61ca7ae48b8f
Gaurav Nelson <gaurav@stackrox.com>
2019-03-28 12:39:52 +1000

    Gather errors from all files
    git tests

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,16 +7,27 @@ set -eu
 wget https://github.com/errata-ai/vale/releases/download/v1.3.2/vale_1.3.2_Linux_64-bit.tar.gz
 tar -xvzf vale_1.3.2_Linux_64-bit.tar.gz vale
 
-#find modified files in last commit and only run vale on modified files
+# Save output in a file
+touch output.txt
 
+# git test
+git branch
+git status
+
+#find modified files in last commit and only run vale on modified files
 for i in $(git diff --name-only "$(git rev-parse HEAD~1)") ; do
 #  fileList[$N]="$i"
   if [ "${i: -3}" == ".md" ] ; then
     echo -e "CHECKING REFERENCES for ${i}"
-    ./vale "${i}"
+    ./vale "${i}" >> output.txt
     echo $'\n'
     (( N= N + 1 ))
   else
-    echo "No modified markdown files to check."
+    echo "Ignored ${i}, not a markdown file."
   fi
 done
+
+if [ -s output.txt ] 
+then
+ cat output.txt
+fi
